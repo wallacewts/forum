4ba0eb4a071546c7e17d02542d3b45408df54577
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-28 16:17:59 -0300

    add topico controller for list all data

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+	@Autowired
+	private TopicoRepository topicoRepository;
+
+	@GetMapping
+	public List<TopicoDto> lista() {
+		List<Topico> topicos = topicoRepository.findAll();
+
+		return TopicoDto.converter(topicos);
+	}
+
+}
