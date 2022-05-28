91309ec31dfabb959aa955c74f85792cc0082b7c
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-28 18:53:39 -0300

    add pagination for list all topics

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,10 +44,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordernacao) {
-
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordernacao);
-
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(page = 0, size = 10) Pageable paginacao) {
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
