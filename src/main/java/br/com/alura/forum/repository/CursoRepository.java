607d014e139c76078fa701f3a79a5fe80129110f
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-28 16:46:29 -0300

    change entity type

--- src/main/java/br/com/alura/forum/repository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -3,9 +3,8 @@ package br.com.alura.forum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
 
-public interface CursoRepository extends JpaRepository<Topico, Long> {
+public interface CursoRepository extends JpaRepository<Curso, Long> {
 
   Curso findByNome(String nome);
   
