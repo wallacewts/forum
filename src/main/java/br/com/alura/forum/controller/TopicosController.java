185e617eff5da9f1d8332eef656da2034cc7f7c9
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-28 16:53:35 -0300

    create save method for topico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,14 +1,21 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
@@ -17,6 +24,9 @@ public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
+
+	@Autowired
+	private CursoRepository cursoRepository;
 
 	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
@@ -30,4 +40,15 @@ public class TopicosController {
 
 	}
 
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriComponentsBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		URI uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		
+		topicoRepository.save(topico);
+		
+		TopicoDto topicoDto = new TopicoDto(topico);
+
+		return ResponseEntity.created(uri).body(topicoDto);
+	}
 }
